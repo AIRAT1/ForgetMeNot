25c4f8b8f5221665949aa2d5c0da1d0f86aa0008
Ayrat <ayrat1@mail.ru>
2018-01-20 00:20:03 +0300

    Complete v3 35.00

--- app/src/main/java/de/android/ayrathairullin/forgetmenot/dialog/AddingTaskDialogFragment.java
+++ app/src/main/java/de/android/ayrathairullin/forgetmenot/dialog/AddingTaskDialogFragment.java
@@ -6,10 +6,14 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -24,13 +28,13 @@ public class AddingTaskDialogFragment extends DialogFragment {
         builder.setTitle(R.string.dialog_title);
 
         View container = inflater.inflate(R.layout.dialog_task, null);
-        TextInputLayout tilTitle = container.findViewById(R.id.tilDialogTaskTitle);
+        final TextInputLayout tilTitle = container.findViewById(R.id.tilDialogTaskTitle);
         TextInputLayout tilDate = container.findViewById(R.id.tilDialogTaskDate);
-        TextInputLayout tilTime = container.findViewById(R.id.tilDialogTaskTime);
+        final TextInputLayout tilTime = container.findViewById(R.id.tilDialogTaskTime);
 
-        EditText etTitle = tilTitle.getEditText();
+        final EditText etTitle = tilTitle.getEditText();
         final EditText etDate = tilDate.getEditText();
-        EditText etTime = tilTime.getEditText();
+        final EditText etTime = tilTime.getEditText();
 
         tilTitle.setHint(getResources().getString(R.string.task_title));
         tilDate.setHint(getResources().getString(R.string.task_date));
@@ -59,6 +63,79 @@ public class AddingTaskDialogFragment extends DialogFragment {
                     }
                 };
                 datePickerFragment.show(getFragmentManager(), "DatePickerFragment");
+            }
+        });
+
+        etTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etTime.length() == 0) {
+                    etTime.setText(" ");
+                }
+
+                DialogFragment timePickerFragment = new TimePickerFragment() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar timeCalendar = Calendar.getInstance();
+                        timeCalendar.set(0, 0, 0, hourOfDay, minute);
+                        etTime.setText(Utils.getTime(timeCalendar.getTimeInMillis()));
+                    }
+
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        etTime.setText(null);
+                    }
+                };
+//                timePickerFragment.show(getFragmentManager(), "TimePickerFragment");
+            }
+        });
+
+        builder.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                final Button positiveButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                if (etTime.length() == 0) {
+                    positiveButton.setEnabled(false);
+                    tilTime.setError(getResources().getString(R.string.dialog_error_empty_title));
+                }
+
+                etTitle.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if (s.length() == 0) {
+                            positiveButton.setEnabled(false);
+                            tilTitle.setError(getResources().getString(R.string.dialog_error_empty_title));
+                        }else {
+                            positiveButton.setEnabled(true);
+                            tilTitle.setErrorEnabled(false);
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
             }
         });
 
