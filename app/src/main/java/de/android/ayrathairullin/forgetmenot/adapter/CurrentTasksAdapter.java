f786855cc475f4ea97cf6add6bcefb84a0d6302b
Ayrat <ayrat1@mail.ru>
2018-01-22 22:50:24 +0300

    complete v8 02.55

--- app/src/main/java/de/android/ayrathairullin/forgetmenot/adapter/CurrentTasksAdapter.java
+++ app/src/main/java/de/android/ayrathairullin/forgetmenot/adapter/CurrentTasksAdapter.java
@@ -62,7 +62,8 @@ public class CurrentTasksAdapter extends TaskAdapter {
             }
 
             itemView.setVisibility(View.VISIBLE);
-            itemView.setBackgroundColor(resources.getColor(R.color.gray_50));
+            taskViewHolder.priority.setEnabled(true);
+//            itemView.setBackgroundColor(resources.getColor(R.color.gray_50));
             taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_default_material_light));
             taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_default_material_light));
             taskViewHolder.priority.setColorFilter(resources.getColor(task.getPriorityColor()));
@@ -85,9 +86,10 @@ public class CurrentTasksAdapter extends TaskAdapter {
             taskViewHolder.priority.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    taskViewHolder.priority.setEnabled(false);
                     task.setStatus(ModelTask.STATUS_DONE);
                     getTaskFragment().activity.dbHelper.update().status(task.getTimeStamp(), ModelTask.STATUS_DONE);
-                    itemView.setBackgroundColor(resources.getColor(R.color.gray_200));
+//                    itemView.setBackgroundColor(resources.getColor(R.color.gray_200));
                     taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_disabled_material_light));
                     taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_disabled_material_light));
                     taskViewHolder.priority.setColorFilter(resources.getColor(task.getPriorityColor()));
